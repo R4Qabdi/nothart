c83fcb8c94122cbbba253a77c0d2a4c9764cd719
R4Qabdi <jadepitkapswen@gmail.com>
2024-10-04 00:14:54 +0800

    main menu

---
+++ scene n script/fmality/mainmenu.gd
@@ -0,0 +1,38 @@
+extends CanvasLayer
+
+var onsav = false
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_start_pressed() -> void:
+	$ani.play("masukstart")
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_option_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_savback_pressed() -> void:
+	onsav = false
+	$ani.play("keluarsav")
+
+func _on_back_pressed() -> void:
+	if onsav :
+		$ani.play("keluarsav")
+	$ani.queue("keluarstart")
+
+func _on_load_pressed() -> void:
+	onsav = true
+	$ani.play("masuksav")
+	
+func _on_startnew_pressed() -> void:
+	get_tree().change_scene_to_file("res://scene n script/devroom/whole.tscn")

0b6e9a585227b5bfd34e002d6c49c509a5cafcce
R4Qabdi <jadepitkapswen@gmail.com>
2024-10-12 08:51:24 +0800

    semoga mantap

--- scene n script/devroom/ui.gd
+++ scene n script/devroom/ui.gd
@@ -7,16 +7,53 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-func _on_button_pressed() -> void:
-	get_tree().quit()
-
 func _on_button_2_pressed() -> void:
 	Dialogic.start("siblok")
-
 
 func _on_serang_focus_exited() -> void:
 	print("kapan sih hilangnya?")
 
-
 func _on_gaje_released() -> void:
 	Input.action_press("serang")
+
+func _on_resume_pressed() -> void:
+	$ani.play_backwards("masuk")
+	menu_on=false
+
+func _on_option_pressed() -> void:
+	$ani.play("opmasuk")
+	opbuka=true
+
+func _on_exitop_pressed() -> void:
+	$ani.play_backwards("opmasuk")
+	opbuka=false
+
+
+func _on_exit_pressed() -> void:
+	get_tree().change_scene_to_file("res://scene n script/fmality/mainmenu.tscn")
+
+func _on_save_pressed() -> void:
+	$ani.play("savmasuk")
+
+func _on_menu_toggled(toggled_on: bool) -> void:
+	if toggled_on :
+		menu_on=true
+		$ani.play("masuk")
+	elif !toggled_on :
+		tutupsemua()
+var menu_on = false
+var savebuka = false
+var opbuka = false
+
+func tutupsemua() -> void:
+	if savebuka :
+		$ani.play_backwards("savmasuk")
+	if opbuka :
+		$ani.play_backwards("opmasuk")
+	$ani.play_backwards("masuk")
+
+func _on_save_toggled(toggled_on: bool) -> void:
+	if toggled_on :
+		$ani.play("savmasuk")
+	elif !toggled_on :
+		$ani.play_backwards("savmasuk")
--- scene n script/devroom/whole.gd
+++ scene n script/devroom/whole.gd
@@ -2,15 +2,12 @@ extends Node2D
 
 func _ready() -> void:
 	pass
-	
 
 func _process(delta: float) -> void:
 	if Input.is_key_pressed(KEY_E):
 		Dialogic.start("siblok")
 	else :
 		pass
-	
 
-
-func _on_button_pressed() -> void:
+func _on_debugworld_pressed() -> void:
 	$WalkerGenerator.generate()
